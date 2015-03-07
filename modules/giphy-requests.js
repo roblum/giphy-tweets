fd2eec08d72165a8bb8ecbe78d023157e6d1fc7e
Matthew Madurski <matt@matthewmadurski.com>
2015-03-07 16:55:00 -0500

    build giphy requests module

--- modules/giphy-requests.js
+++ modules/giphy-requests.js
@@ -1,3 +1,27 @@
-module.exports = function() {
-     console.log('hello from giphy');
+var request = require('request');
+
+
+BASE_URL = 'http://api.giphy.com/v1/gifs';
+API_KEY = 'dc6zaTOxFJmzC';
+
+
+function getGifs(searchTerms, callback) {
+    url = BASE_URL + '/search?q=' + encodeURIComponent(searchTerms)
+        + '&api_key=' + API_KEY;
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            images = parseResponse(body);
+            callback(images);
+        }
+    });
+}
+
+function parseResponse(body) {
+    body = JSON.parse(body);
+    images = [];
+    body.data.forEach(function (image) {
+        images.push(image.images.original.url);
+    });
+    return images;
 }
